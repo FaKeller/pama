e280b3dd6f1e14f05be1c884e05f5ffd0897f5a9
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-04-28 16:10:48 +0200

    added first implementation for secure password

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -17,13 +17,47 @@ function getCurrentTabUrl(callback) {
     // A window can only have one active tab at a time, so the array consists of
     // exactly one tab.
     var tab = tabs[0];
-    var url = tab.url;
-    callback(url);
+    callback(tab.url);
   });
 }
 
-$(document).ready(function () {
-  getCurrentTabUrl(function(url) {
-    $("#siteUrl").val(url);
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length == 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+function secureSecret(secret, domain) {
+  // this function needs to generate a unique result for every site, but
+  // the result should not change over time
+  var secretHash = secret + domain;
+  var sum = 0;
+  var prime = 31;
+  var factor = 1;
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var aLength = alphabet.length;
+  for(var i = 0; i < secretHash.length; i++) {
+    factor = factor * prime % aLength;
+    sum = (sum + factor * secretHash.charAt(i)) % aLength;
+  }
+  return alphabet[sum];
+}
+
+$(document).ready(function() {
+  $("#generateBtn").click(function () {
+    getCurrentTabUrl(function(url) {
+      var secret = $("#yourPassword").val();
+      var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+      var domain = matches && matches[1];
+      if (domain != null) {
+        secret = secureSecret(secret, domain);
+        $("#generatedPassword").html(secret);
+      }
+    });
   });
 });
