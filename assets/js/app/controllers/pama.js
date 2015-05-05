742bdc9cd210191fc7678fa0c942cb85cdf648e7
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-05-05 16:04:38 +0200

    added field for domain input and added angular watch for domain

--- assets/js/app/controllers/pama.js
+++ assets/js/app/controllers/pama.js
@@ -2,8 +2,16 @@ var pamaApp = angular.module('pamaApp', []);
 
 pamaApp.controller('IndexController', function ($scope) {
     $scope.masterPassword = "test";
+    $scope.domain = "http://www.google.de/";
+
     $scope.$watch('masterPassword', function () {
-        var password = $scope.masterPassword;
-        $scope.prefix = secureSecret(password, "domain");
+        $scope.prefix = secureSecret($scope.masterPassword, $scope.domain);
+    }, true);
+
+    $scope.$watch('domain', function () {
+        var matches = $scope.domain.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+        var domain = matches && matches[1];
+        console.log(domain);
+        $scope.prefix = secureSecret($scope.masterPassword, domain);
     }, true);
 });
